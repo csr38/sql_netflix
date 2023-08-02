4ea9c356584110f5502b3d264e4139ad6349ce22
csr38 <cesar1tapia1@gmail.com>
2023-08-01 23:35:49 -0400

    Actores en cada pelicula INNER JOIN

--- consultas.sql
+++ consultas.sql
@@ -6,3 +6,10 @@ SELECT * FROM movies;
 SELECT * FROM actors;
 SELECT * FROM movies_actors;
 SELECT * FROM movies_users;
+
+
+SELECT actors.first_name AS nombre_actor, actors.last_name AS apellido_actor, movies.name_movie AS nombre_pelicula
+FROM movies_actors
+INNER JOIN actors on actors.actor_id =  movies_actors.actor_id
+INNER JOIN movies on movies.movie_id = movies_actors.movie_id;
+
