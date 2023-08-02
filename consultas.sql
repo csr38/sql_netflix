86c72b587a0cef4c3938d92cb94b4db38b75bcdc
csr38 <cesar1tapia1@gmail.com>
2023-08-01 23:58:15 -0400

    Cantidad de peliculas vistas por usuario

--- consultas.sql
+++ consultas.sql
@@ -13,3 +13,8 @@ FROM movies_actors
 INNER JOIN actors on actors.actor_id =  movies_actors.actor_id
 INNER JOIN movies on movies.movie_id = movies_actors.movie_id;
 
+SELECT users.user_handle as Usuario, users.movie_count AS Cuantas_peliculas, movies.name_movie AS NombrePelicula
+FROM movies_users
+INNER JOIN users ON users.user_id = movies_users.user_id
+INNER JOIN movies ON movies.movie_id = movies_users.movie_id
+ORDER BY users.movie_count DESC;
